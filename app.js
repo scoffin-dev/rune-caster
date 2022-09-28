ed4e5e7f77a8d022cc64650578addf81ba7da93f
scoffin-dev <tipsywithwater@gmail.com>
2022-09-28 14:17:20 -0400

    Directory structure update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res) {
-    res.sendFile(`${__dirname}/index.html`);
+    res.sendFile(`${__dirname}/pages/index.html`);
 });
 
 app.use(express.static(`public`));
